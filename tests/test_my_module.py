9d715e226687f52e677c04810a3955103bd796e3
bharadwajsirigadi <105838762+bharadwajsirigadi@users.noreply.github.com>
2023-09-30 11:33:57 -0400

    add tests/ folder

---
+++ tests/test_my_module.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: Your name
+# @Date:   2023-09-30 10:39:16
+# @Last Modified by:   Your name
+# @Last Modified time: 2023-09-30 11:20:17
+# Example test_my_module.py
+# test_my_module.py
+
+from Data2PointCloud.src.datasets.three_d_match import ThreeDMatchDataset2
+
+DATASET_FOLDER_PATH = "/Users/bharadwajsirigadi/Documents/Dataset-Preprocess/custom_dataset" #"/home/bharadwajsirigadi/Documents/Data_preprocess/custom_dataset"
+SEQUENCE = 1
+
+def test_three_d_match():
+    a = ThreeDMatchDataset2(DATASET_FOLDER_PATH, SEQUENCE)
+    points = a[2]
+    print(points)
+    return 
+    
+
+
